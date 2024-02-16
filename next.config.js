65c25cb310f985eff811dbc51ed67c336daaf585
mubeenamin <mubeenamin6@gmail.com>
2024-02-16 13:51:11 +0500

    updated

--- next.config.js
+++ next.config.js
@@ -6,21 +6,21 @@ const nextConfig = {
         source: "/api/:path*",
         destination:
           process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:8001/api/:path*"
+            ? "http://127.0.0.1:8000/api/:path*"
             : "/api/",
       },
       {
         source: "/docs",
         destination:
           process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:8001/docs"
+            ? "http://127.0.0.1:8000/docs"
             : "/api/docs",
       },
       {
         source: "/openapi.json",
         destination:
           process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:8001/openapi.json"
+            ? "http://127.0.0.1:8000/openapi.json"
             : "/api/openapi.json",
       },
     ];
