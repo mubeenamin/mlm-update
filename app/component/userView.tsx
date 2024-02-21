ae2bdfdaa9a50e40ac2640695a0f7697c46075d4
mubeenamin <mubeenamin6@gmail.com>
2024-02-21 09:20:13 +0500

    updated component

--- app/component/userView.tsx
+++ app/component/userView.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React, { useEffect, useState } from "react";
 import {
   Table,
@@ -7,24 +8,24 @@ import {
   TableCell,
   TableHeader,
 } from "@/components/ui/table";
-
+import { Skeleton } from "@/components/ui/skeleton";
+import TableSkeleton from "../(dashboard)/components/tableSkeleton";
 function UserView() {
   const [users, setUsers] = useState([]);
-
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const res = await fetch(
-          "/api/users",{mode:'no-cors'}
-        );
+        const res = await fetch("/api/users", { mode: "no-cors" });
         if (!res.ok) {
           // res.ok returns false if the HTTP status is not 200-299
           throw new Error(`HTTP error! status: ${res.status}`);
         }
         const data = await res.json();
         setUsers(data);
+        setLoading(false);
       } catch (error) {
-        console.log(error);
+        setLoading(false);
       }
     };
     fetchData();
@@ -38,28 +39,32 @@ function UserView() {
   };
   return (
     <main>
-      <Table>
-        <TableHeader>
-          <TableRow>
-            <TableHead>{headName.id}</TableHead>
-            <TableHead>{headName.email}</TableHead>
-            <TableHead>{headName.package}</TableHead>
-            <TableHead>{headName.currency}</TableHead>
-            <TableHead>{headName.pin}</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {users.map((user: any) => (
-            <TableRow key={user.id}>
-              <TableCell>{user.id}</TableCell>
-              <TableCell>{user.email}</TableCell>
-              <TableCell>{user.package_id}</TableCell>
-              <TableCell>{user.currency}</TableCell>
-              <TableCell>{user.pin_id}</TableCell>
+      {loading ? (
+        <TableSkeleton />
+      ) : (
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>{headName.id}</TableHead>
+              <TableHead>{headName.email}</TableHead>
+              <TableHead>{headName.package}</TableHead>
+              <TableHead>{headName.currency}</TableHead>
+              <TableHead>{headName.pin}</TableHead>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
+          </TableHeader>
+          <TableBody>
+            {users.map((user: any) => (
+              <TableRow key={user.id}>
+                <TableCell>{user.id}</TableCell>
+                <TableCell>{user.email}</TableCell>
+                <TableCell>{user.package_id}</TableCell>
+                <TableCell>{user.currency}</TableCell>
+                <TableCell>{user.pin_id}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      )}
     </main>
   );
 }
