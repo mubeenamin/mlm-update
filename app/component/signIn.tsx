044bc6df02165f234397a2ff36c2978de1060eb5
mubeenamin <mubeenamin6@gmail.com>
2024-02-29 21:42:52 +0500

    login page completed

--- app/component/signIn.tsx
+++ app/component/signIn.tsx
@@ -13,32 +13,61 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { z } from "zod";
+import { useRouter } from "next/navigation";
+
 
 const FormSchema = z.object({
-  Email: z.string().min(2, {
-    message: " message: Invalid email address",
-  }),
-  Password: z.string().min(2, {
-    message: " message: Invalid email address",
+  email: z.string().email({ message: "Invalid email address" }),
+  password: z.string().min(2, {
+    message: "Enter your password",
   }),
 });
 
-const onSubmit = () => {
-  console.log("data" );
-};
 
 const SignIn = () => {
+  const router = useRouter();
+
+  const onSubmit = async (data: z.infer<typeof FormSchema>) => {
+    console.log(data);
+    try {
+      const res = await fetch(
+        `/api/login_users?email=${data.email}&password=${data.password}`,
+        {
+          mode: "no-cors",
+        }
+      );
+      if (!res.ok) {
+        // res.ok returns false if the HTTP status is not 200-299
+        throw new Error(`HTTP error! status: ${res.status}`);
+      }
+      const newdata = await res.json();
+      if (data.email === newdata.email && data.password === newdata.password) {
+        console.log("successfull");
+        router.push("/dashboard");
+      } else {
+        console.log("wrong username or password");
+      }
+      handleReset();
+    } catch (error) {}
+  };
+  const handleReset = () => {
+    form.reset({
+      
+      email: "",
+      password: "",
+      
+    });
+  };
   const form = useForm<z.infer<typeof FormSchema>>({
     resolver: zodResolver(FormSchema),
     defaultValues: {
-      Email: "",
-      Password: "",
+      email: "",
+      password: "",
     },
   });
   return (
     <main className="flex min-h-screen flex-col items-center  justify-between">
-      
-      <Form {...form} >
+      <Form {...form}>
         <form
           onSubmit={form.handleSubmit(onSubmit)}
           className="flex flex-col gap-5 p-5 mt-8"
@@ -46,44 +75,42 @@ const SignIn = () => {
           <h1 className="text-4xl text-slate-900 font-medium">Login</h1>
           <FormField
             control={form.control}
-            name="Email"
+            name="email"
             render={({ field }) => (
               <FormItem>
-                <FormLabel >Email</FormLabel>
+                <FormLabel>Email</FormLabel>
                 <FormControl>
                   <Input placeholder="Enter you email" {...field} />
                 </FormControl>
-                
-                <FormMessage />
+
+                <FormMessage className="text-red-500" />
               </FormItem>
-              
             )}
-            
           />
           <FormField
             control={form.control}
-            name="Password"
+            name="password"
             render={({ field }) => (
               <FormItem>
                 <FormLabel>Password</FormLabel>
                 <FormControl>
                   <Input placeholder="Enter Your Password" {...field} />
                 </FormControl>
-                
-                <FormMessage />
+                <FormMessage className="text-red-500" />
               </FormItem>
-              
             )}
-            
           />
-          
-          <Button type="submit" className="border-2 p-2 rounded-xl bg-blue-500 text-slate-800 hover:bg-blue-600 hover:text-slate-900">
+
+          <Button
+            type="submit"
+            className="border-2 p-2 rounded-xl bg-blue-500 text-slate-800 hover:bg-blue-600 hover:text-slate-900"
+          >
             Login
           </Button>
         </form>
       </Form>
     </main>
-  )
-}
+  );
+};
 
-export default SignIn
+export default SignIn;
