fce7892a9655b627e7d1121a6043d9ae4073a4ee
mubeenamin <mubeenamin6@gmail.com>
2024-03-07 15:45:06 +0500

    update

--- app/(userDashboard)/[id]/page.tsx
+++ app/(userDashboard)/[id]/page.tsx
@@ -61,23 +61,23 @@ function Page({ params }: { params: { id: Prop } }) {
       </div>
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
         <Card className="p-8 shadow-md grid grid-cols-3 items-center">
-          <div className="col-span-2">Daily Profit from Package</div>
+          <div className="col-span-2 text-lg">Daily Profit from Package</div>
           <div className="col-span-1 text-end text-4xl">$0</div>
         </Card>
         <Card className="p-8 shadow-md grid grid-cols-3 items-center">
-          <div className="col-span-2">Referrral Profit</div>
+          <div className="col-span-2 text-lg">Referrral Profit</div>
           <div className="col-span-1 text-end text-4xl">$0</div>
         </Card>
         <Card className="p-8 shadow-md grid grid-cols-3 items-center">
-          <div className="col-span-2">Total Earnings</div>
+          <div className="col-span-2 text-lg">Total Earnings</div>
           <div className="col-span-1 text-end text-4xl">$0</div>
         </Card>
         <Card className="p-8 shadow-md grid grid-cols-3 items-center">
-          <div className="col-span-2">Total Referral Count</div>
+          <div className="col-span-2 text-lg">Total Referral Count</div>
           <div className="col-span-1 text-end text-4xl">$0</div>
         </Card>
-        <Card className="p-8 shadow-md grid grid-cols-3 items-center">Total Earning</Card>
-        <Card className="p-8 shadow-md grid grid-cols-3 items-center">Total Profit</Card>
+        <Card className="p-8 shadow-md grid grid-cols-3 items-center text-lg">Total Earning</Card>
+        <Card className="p-8 shadow-md grid grid-cols-3 items-center text-lg">Total Profit</Card>
       </div>
       <div className="">
         <Chart />
