f470b725c565a5017e66ba0a6a1ca4198df21509
mubeenamin <mubeenamin6@gmail.com>
2024-03-07 16:11:51 +0500

    updated

--- app/(userDashboard)/[id]/page.tsx
+++ app/(userDashboard)/[id]/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import Chart from "@/app/component/chart";
-import { Card } from "@/components/ui/card";
+
+import CardsUser from "@/app/component/cards";
 import React, { useEffect, useState } from "react";
 
 type Prop = {
@@ -10,7 +10,7 @@ type Prop = {
 
 function Page({ params }: { params: { id: Prop } }) {
   const userdata = params.id;
-  const userID = Number(userdata);
+  const userID:number = Number(userdata);
   const [users, setUsers] = useState({
     nation_id: "",
     email: "",
@@ -47,42 +47,8 @@ function Page({ params }: { params: { id: Prop } }) {
   }, []);
 
   return (
-    <div className="space-y-4">
-      <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 pb-10">
-        <div>
-          Username:<div>{users.nation_id}</div>
-        </div>
-        <div>
-          Email:<div>{users.email}</div>
-        </div>
-        <div>
-          Package:<div>{users.package}</div>
-        </div>
-      </div>
-      <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
-        <Card className="p-8 shadow-md grid grid-cols-3 items-center">
-          <div className="col-span-2 text-lg">Daily Profit from Package</div>
-          <div className="col-span-1 text-end text-4xl">$0</div>
-        </Card>
-        <Card className="p-8 shadow-md grid grid-cols-3 items-center">
-          <div className="col-span-2 text-lg">Referrral Profit</div>
-          <div className="col-span-1 text-end text-4xl">$0</div>
-        </Card>
-        <Card className="p-8 shadow-md grid grid-cols-3 items-center">
-          <div className="col-span-2 text-lg">Total Earnings</div>
-          <div className="col-span-1 text-end text-4xl">$0</div>
-        </Card>
-        <Card className="p-8 shadow-md grid grid-cols-3 items-center">
-          <div className="col-span-2 text-lg">Total Referral Count</div>
-          <div className="col-span-1 text-end text-4xl">$0</div>
-        </Card>
-        <Card className="p-8 shadow-md grid grid-cols-3 items-center text-lg">Total Earning</Card>
-        <Card className="p-8 shadow-md grid grid-cols-3 items-center text-lg">Total Profit</Card>
-      </div>
-      <div className="">
-        <Chart />
-      </div>
-    </div>
+    <><CardsUser users={users}/></>
+    
   );
 }
 
