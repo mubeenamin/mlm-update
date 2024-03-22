3bce47c586b0737d7e1b4eeb321617ef651e8dbd
mubeenamin <mubeenamin6@gmail.com>
2024-03-22 13:54:48 +0500

    update frontend dashboard

--- app/component/cards.tsx
+++ app/component/cards.tsx
@@ -17,24 +17,24 @@ function CardsUser({ users }: any) {
         </div>
       </div>
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
-        <Card className="p-8 shadow-md grid grid-cols-3 items-center">
-          <div className="col-span-2 text-lg">Daily Profit from Package</div>
-          <div className="col-span-1 text-end text-4xl">{users.balance}</div>
+        <Card className="p-8 shadow-md grid items-center gap-8">
+          <div className="col-span-2 text-lg">Daily Profit From Package</div>
+          <div className="col-span-1 text-4xl">${users.balance}</div>
         </Card>
-        <Card className="p-8 shadow-md grid grid-cols-3 items-center">
+        <Card className="p-8 shadow-md grid items-center gap-8">
           <div className="col-span-2 text-lg">Referrral Profit</div>
-          <div className="col-span-1 text-end text-4xl">$0</div>
+          <div className="col-span-1  text-4xl">$0</div>
         </Card>
-        <Card className="p-8 shadow-md grid grid-cols-3 items-center">
+        <Card className="p-8 shadow-md grid  items-center gap-8">
           <div className="col-span-2 text-lg">Total Earnings</div>
-          <div className="col-span-1 text-end text-4xl">$0</div>
+          <div className="col-span-1  text-4xl">$0</div>
         </Card>
-        <Card className="p-8 shadow-md grid grid-cols-3 items-center">
+        <Card className="p-8 shadow-md grid  items-center gap-8">
           <div className="col-span-2 text-lg">Total Referral Count</div>
-          <div className="col-span-1 text-end text-4xl">$0</div>
+          <div className="col-span-1  text-4xl">$0</div>
         </Card>
-        <Card className="p-8 shadow-md grid grid-cols-3 items-center text-lg">Total Earning</Card>
-        <Card className="p-8 shadow-md grid grid-cols-3 items-center text-lg">Total Profit</Card>
+        <Card className="p-8 shadow-md grid  items-center text-lg">Total Earning</Card>
+        <Card className="p-8 shadow-md grid  items-center text-lg">Total Profit</Card>
       </div>
       <div className="">
         <Chart />
