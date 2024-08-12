5bccda05f5f879bcdaea030a04d27097912a15b8
Muhammad Waheed <115403188+Waheedsadique@users.noreply.github.com>
2024-08-12 14:42:01 +0500

    h

--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -1,18 +1,11 @@
 import React from "react";
 import UserView from "@/app/component/userView";
 
-<<<<<<< HEAD
-function Dashboard() {
+export default function Dashboard() {
   return (
     <div className="h-[calc(100vh-96px] rounded-lg p-4 bg-white">
      <UserView/>
     </div>
   );
 }
-=======
-import { getUser } from "@/lib/apiCalling";
->>>>>>> c6427d8b177f041f6aa8c3a945361acfd260e794
 
-export default async function Dashboard() {
-  return <UserView />;
-}
