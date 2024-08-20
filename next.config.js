8f00ca1293bfbf8d6e8835396e5ad2d0d0ee74a2
mubeenamin <mubeenamin6@gmail.com>
2024-08-20 20:53:07 +0500

    updated fastapi routes for vercel

--- next.config.js
+++ next.config.js
@@ -21,21 +21,21 @@ const nextConfig = {
         destination:
           process.env.NODE_ENV === "development"
             ? "http://127.0.0.1:8000/fastapi/api/:path*"
-            : "/fastapi/api/",
+            : "/fastapi/api/:path*",
       },
       {
         source: "/docs",
         destination:
           process.env.NODE_ENV === "development"
             ? "http://127.0.0.1:8000/docs"
-            : "/fastapi/docs",
+            : "/fastapi/api/docs",
       },
       {
         source: "/openapi.json",
         destination:
           process.env.NODE_ENV === "development"
             ? "http://127.0.0.1:8000/openapi.json"
-            : "/api/openapi.json",
+            : "/fastapi/api/openapi.json",
       },
     ];
   },
