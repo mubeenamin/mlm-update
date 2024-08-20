32afd83c2ed84cbb98fd87a356e51996c4b6a63d
mubeenamin <mubeenamin6@gmail.com>
2024-08-20 22:08:25 +0500

    u

--- next.config.js
+++ next.config.js
@@ -1,19 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  webpack: (config, { isServer }) => {
-    // Fixes npm packages that depend on `child_process` module
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        child_process: false,
-      };
-    }
-
-    return config;
-  },
-
   rewrites: async () => {
     return [
       {
