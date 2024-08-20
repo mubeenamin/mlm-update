a9170a809a9e85e8428080cfd123346c2f142e9b
mubeenamin <mubeenamin6@gmail.com>
2024-08-20 20:26:35 +0500

    updated

--- next.config.js
+++ next.config.js
@@ -28,7 +28,7 @@ const nextConfig = {
         destination:
           process.env.NODE_ENV === "development"
             ? "http://127.0.0.1:8000/docs"
-            : "/api/docs",
+            : "/fastapi/docs",
       },
       {
         source: "/openapi.json",
