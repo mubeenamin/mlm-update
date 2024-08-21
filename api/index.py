3d96dc12214daec2d9bacbb0b47253c2f719a110
mubeenamin <mubeenamin6@gmail.com>
2024-08-21 09:13:55 +0500

    up

--- api/index.py
+++ api/index.py
@@ -71,7 +71,7 @@ def main(db: Session):
 
 def start_scheduler():
     scheduler = BackgroundScheduler()
-    scheduler.add_job(run_main, 'interval', seconds=300)
+    scheduler.add_job(run_main, 'interval', seconds=86400)
     scheduler.start()
 
 def run_main():
