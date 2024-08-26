87373ab5ae909516f6ed38f73444cb602a9b6909
mubeenamin <mubeenamin6@gmail.com>
2024-08-26 18:49:51 +0500

    created message model

--- api/models.py
+++ api/models.py
@@ -139,6 +139,11 @@ class Referral(SQLModel, table=True):
     referrer: User = Relationship(back_populates="referrals", sa_relationship_kwargs={"foreign_keys": "[Referral.referrer_user_id]"})
     
 
+class Message(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    sender_id: int = Field(foreign_key="user.id")
+    recipient_id: int = Field(foreign_key="user.id")
+    content: str
 
 
 class Token(SQLModel):
