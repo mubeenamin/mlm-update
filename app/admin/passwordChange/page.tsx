a4733a06ec02becdf1f46469cd2c638ca17fb32f
mubeenamin <mubeenamin6@gmail.com>
2025-02-17 13:16:31 +0500

    update admin

--- app/admin/passwordChange/page.tsx
+++ app/admin/passwordChange/page.tsx
@@ -1,8 +1,11 @@
 import ChangePassword from "@/app/component/changePassword";
+import PasswordChangeWithOTP from "@/app/component/PasswordChangeWithOTP";
 import React from "react";
 
 export default function PasswordChange() {
-  return <div className='bg-white h-[calc(100vh-96px] rounded-lg p-4'>
-    <ChangePassword/>
-  </div>;
+  return (
+    <div className="bg-white h-[calc(100vh-96px] rounded-lg p-4">
+      <PasswordChangeWithOTP />
+    </div>
+  );
 }
